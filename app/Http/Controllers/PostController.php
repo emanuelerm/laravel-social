023569546905bc5f385de39f2714d12992bba039
emanuelerm <emanueler00@gmail.com>
2023-06-02 16:30:32 +0200

    mcs

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    //
+}
