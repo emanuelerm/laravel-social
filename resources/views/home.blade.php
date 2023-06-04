f78371aa6df69592c77e2bbf2356d96ce297425f
MatteoAgu01 <matteoaguiari012@gmail.com>
2023-06-04 14:55:33 +0200

    add figate assurde

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.app')
 @section('content')
-	
+	<div class="my-container">
+		<svg class="my-svg" viewBox="0 0 960 300">
+			<symbol id="s-text">
+				<text 
+				text-anchor="middle" x="50%" y="80%">
+				Laravel Social
+				</text>
+			</symbol>
+		
+			<g class="g-ants">
+				<use xlink:href="#s-text" class="text-copy"></use>
+				<use xlink:href="#s-text" class="text-copy"></use>
+				<use xlink:href="#s-text" class="text-copy"></use>
+				<use xlink:href="#s-text" class="text-copy"></use>
+				<use xlink:href="#s-text" class="text-copy"></use>
+			</g>
+		</svg>
+	</div>
 @endsection
-
-
-{{-- /image/chri-omo.jpg
-    /image/teo_agu.jpg
-    /image/emanuelem-gay.jpg
-    /image/matteo-il-figo.jpg
-    /image/emanuelem-barba.jpg --}}
